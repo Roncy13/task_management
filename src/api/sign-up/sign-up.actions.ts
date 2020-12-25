9bde55b505ad524f3dadd22c70f7517fe97bacf3
Mejas CesRon <JamesRoncy13@gmail.com>
2020-12-25 20:01:42 +0800

    move action custom code in the custom folder

--- src/api/sign-up/sign-up.actions.ts
+++ src/api/sign-up/sign-up.actions.ts
@@ -3,9 +3,9 @@ import SmurfResponse, { SmurfAction } from '../../core/response';
 @SmurfAction({
   action: '/sign-up',
 })
-export class SignupApi extends SmurfResponse {
+export class SignUpApi extends SmurfResponse {
 
   async run() {
-    this.data = 'Index Api for signUp';
+    this.data = 'Index Api for sign-up';
   }
 }
---
+++ src/core/custom/actions/index.ts
@@ -0,0 +1,90 @@
+import { Choices } from "../../generator/utils";
+import { ActionConfig } from "plop";
+
+export const apiPath = '{{HlprApiDirectory name}}/{{DashCase name}}.actions.ts';
+
+export const DirectoryType = (type = '') => ({
+  name: 'type',
+  type: 'list',
+  choices: Object.values(Choices),
+  message: 'Please Choose what directory type'
+});
+
+export const DirectoryTypeForActions = (type = '') => ({
+  name: 'type',
+  type: 'list',
+  choices: Object.values(Choices).filter(f => f !== Choices.Global),
+  message: 'Please Choose what directory type'
+});
+
+export const GenerateComponent: any = {
+  description: 'Generator For Creating API Component, Smurf.',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Name of Api Component you want to create.'
+    },
+    DirectoryTypeForActions()
+  ],
+  actions: [
+    {
+      type: 'addMany',
+      base: 'src/api/',
+      destination: '{{name}}',
+      templateFiles: '../../smurf-templates/*'
+    }
+  ]
+};
+
+export const GenerateAction: any =  {
+  description: 'Generator For Creating API Component, Smurf.',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Name of Action you want to create.'
+    },
+    DirectoryTypeForActions()
+  ],
+  actions: ({ type }: ActionConfig) => {
+    const path = type === Choices.Default ? `{{HlprApiDirectory name}}/{{DashCase name}}.actions.ts` : apiPath;
+    const actions = [
+      {
+        type: 'pathDirectory',
+      },
+      {
+        type: 'add',
+        path,
+        templateFile: '{{HlprBaseDirectory}}/smurf-templates/action.smurf'
+      },
+      {
+        type: 'modify',
+        path,
+        pattern: /SmurfApi/i,
+        template: '{{ApiChangeIndexApiName name}}'
+      },
+      {
+        type: 'modify',
+        path,
+        pattern: /Smurf Api Data/i,
+        template: '{{ApiChangeApiData name}}'
+      },
+      {
+        type: 'modify',
+        path,
+        pattern: /custom-end-point/i,
+        template: '{{ApiFileName name}}'
+      }
+    ];
+    return actions;
+  }
+};
+
+export default {
+  apiPath,
+  DirectoryType,
+  DirectoryTypeForActions,
+  GenerateAction,
+  GenerateComponent
+};
