daf12ae76da139ba03d9daafd08ad671b1d96eba
James F. Roncesvalles <50896886+Roncy13@users.noreply.github.com>
2021-01-28 22:30:39 +0800

    change statusCode to FORBIDDEN

--- src/core/guard.error.ts
+++ src/core/guard.error.ts
@@ -4,13 +4,13 @@ import { StatusCodes } from 'http-status-codes';
 
 export default class GuardError extends Error {
   errorParams: IError;
-  statusCode: number = StatusCodes.UNPROCESSABLE_ENTITY;
+  statusCode: number = StatusCodes.FORBIDDEN;
 
   constructor (params : IPolicy) {
     super(params.message)
     // assign the error class name in your custom error (as a shortcut)
     this.errorParams = params;
     this.name = params.name || "Guarded Route";
-    this.statusCode = params.statusCode || StatusCodes.UNPROCESSABLE_ENTITY
+    this.statusCode = params.statusCode || StatusCodes.FORBIDDEN
   }
 }
